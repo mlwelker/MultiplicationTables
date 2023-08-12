8cc075a8287b600fd1524b5724d118506bcdcc22
mlwelker <nutria.watts.0g@icloud.com>
2023-08-12 17:45:19 -0500

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Michael Welker on 2023-08-12.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
